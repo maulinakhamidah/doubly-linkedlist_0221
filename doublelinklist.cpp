d4cc1694e283dc2beb19a76d6a7cca574159e0c0
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-13 12:07:01 +0700

    membuat prosedur traverse untuk menampilkan data terbalik

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -136,8 +136,126 @@ void deleteNode()
 
     // Node to be delete in the first node
     if (current = START)
-    
+      {
+        START = START->next; // step 2: update the START pointer
+        if (START != NULL)
+        {
+            START->prev = NULL;
+        }
+
+      }
+      else
+      {
+        // node to be deleted is not the first node
+        previous->next = current->next;
+        if (current-> != NULL)
+        {
+            // if 
+            current->next->prev = previous;
+
+        }
+      }
+      // release the memory of the node marked as current
+      delete current;
+      cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
+      }
+
+      // method untuk mengecek apakah list kosong
+      bool listEmpaty()
+      {
+        return (START == NULL);
+      }
+
+      // prosedur traverse untuk menampilkan data urutan terbalik
+      void revtraverse()
+      {
+        if (listEmpaty)
+        cout <<"\nRecords in descending order of roll number are:" << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next
+
+            while (currentNode !=  NULL)
+            {
+                cout << currentNode->noMhs << " " << currentNode->name << endl;
+                currentNode = currentNode->prev;
+            }
+      }
 
 }
 
+// prosedur untuk mencari data dan menapilkan data yang dicari jika ada
+void searchData()
+{
+    if (listEmpaty() == true)
+    {
+        cout << "\nList is empaty" << endl;
+    }
+    Node *prev, *curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search: " ;
+    int num;
+    cin >> num;
+    if (search(num,&prev, &curr) == false)
+       cout << "\nRecord not found" << endl;
 
+    else
+    {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number:" << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
+}
+
+int main()
+{
+    // perulangan selama bernilai benar untuk program utama double likedlist
+    while (true)
+    {
+        try
+        {
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record to the list" << endl;
+            cout << "2. Delete a record from the list " << endl;
+            cout << "3. view all record in the ascending order of roll numbers"  << endl;
+            cout << "4. view all record in the ascending order of roll numbers" << endl;
+            cout << "5. search for a record in the list" << endl;
+            cout << "6. keluar" << endl;
+            cout << " Masukkan pilihan (1-6):";
+            char ch;
+            cin >> ch;
+
+            switch (ch)
+            {
+                case '1':
+                addNode();
+                break;
+                case '2':
+                deleteNode();
+                break;
+                case '3'
+                traverse();
+                break;
+                case '4':
+                revtraverse();
+                break;
+                case '5':
+                searchData();
+                break;
+                case '6':
+                return 0;
+                default:
+                cout << "\nInvalid option" << endl;
+                break;
+
+
+            }
+        }
+        catch (exception &e)
+        {
+            cout << " Check for the values entered." << endl;
+        }
+        
+    }
+
+}
